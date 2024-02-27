ba2595006ba6a007e2c467a95c7c26c14744f805
Christopher Juwao <njuwao@icloud.com>
2024-02-27 20:36:22 +0200

    Get schools query

--- app/models/school.server.ts
+++ app/models/school.server.ts
@@ -1,6 +1,39 @@
-import { client } from "#app/utils/db.server"
-import e from "#dbschema/edgeql-js/index"
+import * as edgedb from "edgedb"
+import { School } from "../types"
 
-const School = { name: "Anxend School" } // Just an example this won't actually run unless it was called.
+const client = edgedb.createClient()
 
-const newSchool = await e.insert(e.School, School).run(client)
+async function querySchools() {
+  const data = await client.query<School[]>(
+    `select default::School {
+      name,
+      id,
+      emailDomain,
+      juniorSchool,
+      seniorSchool,
+      address: {
+        updatedAt,
+        createdAt,
+        id,
+        addressCode,
+        country,
+        nameOrNumber,
+        region,
+        street2,
+        street,
+        townOrCity
+      }
+    }`,
+  )
+
+  return data
+}
+
+export async function getSchools() {
+  try {
+    const data = await querySchools()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
