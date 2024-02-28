0b25682e3254757c20447966eda9b631d9bbb559
Christopher Juwao <njuwao@icloud.com>
2024-02-28 13:52:01 +0200

    get schools query

---
+++ app/queries/index.ts
@@ -0,0 +1,38 @@
+import * as edgedb from "edgedb"
+import { School } from "../types"
+
+const client = edgedb.createClient()
+
+async function querySchools() {
+  const data = await client.query<School>(
+    `select default::School {
+      name,
+      id,
+      emailDomain,
+      juniorSchool,
+      seniorSchool,
+      address: {
+        updatedAt,
+        createdAt,
+        id,
+        addressCode,
+        country,
+        nameOrNumber,
+        region,
+        street2,
+        street,
+        townOrCity
+      }
+    }`,
+  )
+  return data
+}
+
+export async function getSchools() {
+  try {
+    const data = await querySchools()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
